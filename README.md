c3bc46a7f1d2496194eadf7af7927d79d82f726f
Josh Brown <jobr3255@colorado.edu>
2020-03-28 20:32:16 -0600

    Added install script and updated readme

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-## API
+# CU HvZ
+
+## Installing
+Run the installation script as sudo to install all the dependencies
+```
+$ sudo ./install.sh
+```
+## Setting up database
+Import the database
+```
+$ mysql -u root -p < CUHvZ.sql
+```
+
+### API
 Start the api with `npm start` command in the `CUHvZ/api/` directory
 
 ### Client
---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+ERROR='\e[38;5;1mERROR:\e[38;5;231m'
+WARN='\e[38;5;11mWARN:\e[38;5;231m'
+INFO='\e[38;5;38mINFO:\e[38;5;231m'
+
+SUCCESS='\e[38;5;2m'
+RESET='\e[38;5;231m'
+
+node -v > /dev/null 2>&1
+if [[ "$?" -ne 0 ]] ; then
+  echo -e $INFO 'Installing node';
+  apt install -y nodejs > /dev/null 2>&1
+  if [[ "$?" -ne 0 ]] ; then
+    echo -e $ERROR 'Failed to install node';
+  else
+    echo -e $INFO $SUCCESS"nodejs installed" $RESET
+  fi
+fi
+
+npm -v > /dev/null 2>&1
+if [[ "$?" -ne 0 ]] ; then
+  echo -e $INFO 'Installing npm';
+  apt install -y npm > /dev/null 2>&1
+  if [[ "$?" -ne 0 ]] ; then
+    echo -e $ERROR 'Failed to install npm';
+  else
+    echo -e $INFO $SUCCESS"nodejs installed" $RESET
+  fi
+fi
+
+mysql -V > /dev/null 2>&1
+if [[ "$?" -ne 0 ]] ; then
+  echo -e $INFO 'Installing mysql-server';
+  apt install -y mysql-server > /dev/null 2>&1
+  if [[ "$?" -ne 0 ]] ; then
+    echo -e $ERROR 'Failed to install mysql-server';
+  else
+    systemctl enable mysql.service
+    echo -e $INFO $SUCCESS"mysql-server installed" $RESET
+  fi
+fi
+
+echo -e $SUCCESS"Done" $RESET
