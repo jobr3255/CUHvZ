eee5acffa6cf1ff7fcdba9c9f916901700dbbc50
Josh Brown <jobr3255@colorado.edu>
2020-03-29 10:34:50 -0600

    Added rules page

--- client/src/components/layout/Routes.js
+++ client/src/components/layout/Routes.js
@@ -5,12 +5,14 @@ import {
 } from "react-router-dom";
 
 import Home from '../../pages/Home.js';
+import Rules from '../../pages/Rules.js';
 
 export default class Routes extends Component {
   render() {
     return(
       <Switch>
         <Route path="/" exact     component={Home}/>
+        <Route path="/rules"      component={Rules}/>
       </Switch>
     );
   }
