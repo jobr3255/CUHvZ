93924937503f639d4ae9e0da80cf02e996982a67
Josh Brown <jobr3255@colorado.edu>
2020-04-05 12:43:41 -0600

    built form component

---
+++ client/src/components/Form/Form.tsx
@@ -0,0 +1,36 @@
+import React, { FormEvent } from 'react';
+
+interface FormProps {
+  onSubmit: (data: { [key: string]: string | number }) => void;
+}
+
+export default class Form extends React.Component<FormProps> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      callback: props.onSubmit
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    var form = (e.target as HTMLElement);
+    var inputs = form.getElementsByTagName("input");
+    var element;
+    var data: { [key: string]: string | number } = {};
+    for (let i = 0; i < inputs.length; ++i) {
+      element = (inputs[i] as HTMLInputElement);
+      data[element.name] = element.value;
+    }
+    this.props.onSubmit(data);
+  }
+
+  render() {
+    return (
+      <form onSubmit={e => this.handleSubmit(e)} action="post" >
+        {this.props.children}
+      </form>
+    );
+  }
+}
