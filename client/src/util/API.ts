41abb5cb4421dba42f3e93e08647e3939944bc66
Josh Brown <jobr3255@colorado.edu>
2020-04-05 14:33:02 -0600

    added API class

---
+++ client/src/util/API.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+export default class API {
+  static get(url: string) {
+    return new Promise(function(resolve, reject) {
+      axios({
+        method: 'GET',
+        url: url
+      })
+        .then(function(response: any) {
+          resolve(response);
+        })
+        .catch(function(error: any) {
+          reject(error);
+        });
+    });
+  }
+
+  static post(url: string, data: JSON) {
+    return new Promise(function(resolve, reject) {
+      axios({
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        data: data,
+        url: url
+      })
+        .then(function(response) {
+          resolve(response);
+        })
+        .catch(function(error) {
+          reject(error);
+        });
+    });
+  }
+}
